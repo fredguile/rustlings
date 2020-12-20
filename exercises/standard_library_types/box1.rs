a64132b073b6d64a571c24b51e8c2d1df26c18fe
Fred Ghilini <fredguile@gmail.com>
2020-12-20 11:35:57 +1100

    Completed some

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,25 +16,28 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
+use List::{Cons, Nil};
+
 fn main() {
     println!("This is an empty cons list: {:?}", create_empty_list());
-    println!("This is a non-empty cons list: {:?}", create_non_empty_list());
+    println!(
+        "This is a non-empty cons list: {:?}",
+        create_non_empty_list()
+    );
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    Cons(1, Box::new(Nil))
 }
 
 #[cfg(test)]
